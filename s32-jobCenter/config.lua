c3576cc13fe43e7bd26a361a6fa57d2ccd56e8b9
spiry32 <103727459+spiry32@users.noreply.github.com>
2022-10-03 17:09:58 +0300

    Create config.lua

---
+++ s32-jobCenter/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.npcModel = "a_m_m_prolhost_01"
+Config.blipEnabled = true
+Config.Text = "Apasa ~g~[E]~w~ pentru a deschide Job Center-ul"
+Config.blipSprite = 408
+Config.blipColor = 11
+Config.NPCPos = vector4(-264.39, -965.19, 30.22, 207.32)
+Config.Pos = vector3(-264.39, -965.19, 32.22)
